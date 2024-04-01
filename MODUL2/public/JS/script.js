e66eba6760a4d923f50cc7362197a48361a94eca
muhamad ridwan <riduanlgs038@gmail.com>
2024-04-01 21:40:53 +0700

    commit kedua

--- MODUL2/public/JS/script.js
+++ MODUL2/public/JS/script.js
@@ -1,2 +1,41 @@
 
+    function validateForm() {
+        // Reset errors
+        document.getElementById("usernameError").innerHTML = "";
+        document.getElementById("passwordError").innerHTML = "";
+
+        // Get input values
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+
+        var valid = true;
+
+        // Validate username length
+        if (username.length > 7) {
+            document.getElementById("usernameError").innerHTML = "Username tidak boleh lebih dari tujuh karakter.";
+            valid = false;
+        }
+
+        // Validate password length and complexity
+        if (password.length < 10) {
+            document.getElementById("passwordError").innerHTML = "Password harus terdiri dari minimal sepuluh karakter.";
+            valid = false;
+        } else if (!password.match(/[A-Z]/) || !password.match(/[a-z]/) || !password.match(/[0-9]/) || !password.match(/[!@#$%^&*()_+]/)) {
+            document.getElementById("passwordError").innerHTML = "Password harus terdiri dari huruf kapital, huruf kecil, angka, dan karakter khusus.";
+            valid = false;
+        }
+
+
+
+        return valid;
+    }
+
+    function togglePassword() {
+        var passwordField = document.getElementById("password");
+        if (passwordField.type === "password") {
+            passwordField.type = "text";
+        } else {
+            passwordField.type = "password";
+        }
+    }
 
--- MODUL2/resources/views/Modul2.blade.php
+++ MODUL2/resources/views/Modul2.blade.php
@@ -6,25 +6,19 @@
     <title>Form Registrasi</title>
     <link rel="stylesheet" href="CSS/index.css">
     <link rel="stylesheet" href="CSS/style.css">
+    <script src="{{ asset('JS/script.js') }}"></script>
+
     <style>
-
-
-
-
-        .error {
-            color: red;
-            margin-top: -10px;
-            margin-bottom: 10px;
-
-        }
-
-        .show-password {
-            cursor: pointer;
-            font-size: 12px;
-            margin-left: 5px;
-        }
-
+    .error {
+    color: red;
+    margin-top: -10px;
+    margin-bottom: 10px;
+    }
     </style>
+
+
+
+
 </head>
 <body>
     <div class="container">
@@ -58,46 +52,7 @@
         </form>
     </div>
 
-    <script>
-        function validateForm() {
-            // Reset errors
-            document.getElementById("usernameError").innerHTML = "";
-            document.getElementById("passwordError").innerHTML = "";
 
-            // Get input values
-            var username = document.getElementById("username").value;
-            var password = document.getElementById("password").value;
-
-            var valid = true;
-
-            // Validate username length
-            if (username.length > 7) {
-                document.getElementById("usernameError").innerHTML = "Username tidak boleh lebih dari tujuh karakter.";
-                valid = false;
-            }
-
-            // Validate password length and complexity
-            if (password.length < 10) {
-                document.getElementById("passwordError").innerHTML = "Password harus terdiri dari minimal sepuluh karakter.";
-                valid = false;
-            } else if (!password.match(/[A-Z]/) || !password.match(/[a-z]/) || !password.match(/[0-9]/) || !password.match(/[!@#$%^&*()_+]/)) {
-                document.getElementById("passwordError").innerHTML = "Password harus terdiri dari huruf kapital, huruf kecil, angka, dan karakter khusus.";
-                valid = false;
-            }
-
-
-
-            return valid;
-        }
-
-        function togglePassword() {
-            var passwordField = document.getElementById("password");
-            if (passwordField.type === "password") {
-                passwordField.type = "text";
-            } else {
-                passwordField.type = "password";
-            }
-        }
-    </script>
 </body>
+
 </html>
