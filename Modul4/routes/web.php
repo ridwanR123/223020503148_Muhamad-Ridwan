4915820a56954d35deb9a2c86952f05bcd56691d
muhamad ridwan <riduanlgs038@gmail.com>
2024-04-21 17:10:51 +0700

    commit pertama

---
+++ Modul4/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('/posts', App\Http\Controllers\PostController::class);
+
+
+
